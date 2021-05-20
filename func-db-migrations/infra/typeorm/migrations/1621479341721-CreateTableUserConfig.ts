e78d6183352c06d8079cd1a0a4fe2f4fb657821c
Leonardo Mendes <lsm.ccomp@gmail.com>
2021-05-20 15:04:06 -0300

    Adicionando migrations para user_config

---
+++ func-db-migrations/infra/typeorm/migrations/1621479341721-CreateTableUserConfig.ts
@@ -0,0 +1,51 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateTableUserConfig1621479341721 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'user_config',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'integer',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment' 
+                    },
+                    {
+                        name: 'display_name',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'emailVerify',
+                        type: 'boolean',
+                        default: false
+                    },
+                    {
+                        name: 'position',
+                        type: 'varchar',
+                        enum: ['client', 'employee', 'admin']
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('user_config')
+    }
+
+}
