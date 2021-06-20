4e12b37911536ec00cb6c6f0d4766e1ea91753ef
Leonardo Mendes <lsm.ccomp@gmail.com>
2021-06-20 20:31:48 -0300

    :rocket: Adiciona método updateStagePrice

---
+++ func-price/updateStagePrice/index.ts
@@ -0,0 +1,49 @@
+import 'reflect-metadata';
+import { container } from 'tsyringe';
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { MemberNotAuthenticatedException } from "../shared/exception/memberNotAuthenticatedexception";
+import { RequestValidation } from "../shared/utils/requestValidation";
+import UpdateStagePriceService from './services/updateStagePriceService';
+import * as path from 'path';
+import '../container';
+
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const responseSchemaValidation = RequestValidation.validate(context, req, path.join(__dirname, './schemas/requestDefinition.json'));
+    if (responseSchemaValidation.status == 400) {
+        context.res = responseSchemaValidation;
+        return;
+    }
+    
+    try {
+        if(!req.headers['authorization']) {
+            throw new MemberNotAuthenticatedException();
+        }
+            
+        let accessToken = req.headers.authorization.split(' ')[1];
+
+        const updateStagePrice = container.resolve(UpdateStagePriceService);
+        const StagePrice = await updateStagePrice.execute(req.body, req.params.id, accessToken);
+        context.res = {
+            status: StagePrice.status,
+            body: StagePrice.data
+        };
+    } catch (error) {
+        if (error.code === "400") {
+            error.data = {
+                event: {
+                    code: error.code,
+                    type: error.eventType,
+                    message: error.message,
+                    details: error.detail
+                }
+            }
+        }
+        context.res = {
+            status: error.status ? error.status : 500,
+            body: error.data
+        };
+    }
+};
+
+export default httpTrigger;
