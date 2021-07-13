72868035c096589221030c8e3728b1a62a1bd807
Leonardo Mendes <lsm.ccomp@gmail.com>
2021-07-13 17:55:37 -0300

    :gift: Adiciona nova migration para o stage

---
+++ func-db-migrations/infra/typeorm/migrations/1626209130943-CreateTableCreateStage.ts
@@ -0,0 +1,55 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export class CreateTableCreateStage1626209130943 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'stage',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'integer',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment' 
+                    },
+                    {
+                        name: 'construction_id',
+                        type: 'integer'
+                    },
+                    {
+                        name: 'stage_name',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'creation_date',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'last_update',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                ]
+            })
+        )
+
+        await queryRunner.createForeignKey(
+            'stage',
+            new TableForeignKey({
+                name: 'stage_FK_construction_config',
+                columnNames: ['construction_id'],
+                referencedTableName: 'construction_config',
+                referencedColumnNames: ['id']
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey('stage', 'stage_FK_construction_config');
+        await queryRunner.dropTable('stage');
+    }
+}
