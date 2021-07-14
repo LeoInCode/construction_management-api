5f4c45bb9554d6653d4ba150fd91f0606f188911
Leonardo Mendes <lsm.ccomp@gmail.com>
2021-07-13 21:26:13 -0300

    :rocket: Adiciona nova migration para activity

---
+++ func-db-migrations/infra/typeorm/migrations/1626221537777-CreateTableActivity.ts
@@ -0,0 +1,70 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export class CreateTableActivity1626221537777 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'activity',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'integer',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment' 
+                    },
+                    {
+                        name: 'construction_id',
+                        type: 'integer'
+                    },
+                    {
+                        name: 'stage_id',
+                        type: 'integer',
+                    },
+                    {
+                        name: 'activity_name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'creation_date',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'last_update',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                ]
+            })
+        )
+
+        await queryRunner.createForeignKey(
+            'activity',
+            new TableForeignKey({
+                name: 'activity_FK_construction_config',
+                columnNames: ['construction_id'],
+                referencedTableName: 'construction_config',
+                referencedColumnNames: ['id']
+            })
+        )
+
+        await queryRunner.createForeignKey(
+            'activity',
+            new TableForeignKey({
+                name: 'activity_FK_stage',
+                columnNames: ['stage_id'],
+                referencedTableName: 'stage',
+                referencedColumnNames: ['id']
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey('activity', 'activity_FK_construction_config');
+        await queryRunner.dropForeignKey('activity', 'activity_FK_stage');
+        await queryRunner.dropTable('activity');
+    }
+
+}
