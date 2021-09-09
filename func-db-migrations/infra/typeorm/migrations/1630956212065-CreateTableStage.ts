627e3575f35d9281478300816dbffc9ae0ae1536
Leonardo Mendes <lsm.ccomp@gmail.com>
2021-09-09 17:06:29 -0300

    :bug: Corrige nome da migration

--- func-db-migrations/infra/typeorm/migrations/1630956212065-CreateTableStage.ts
+++ func-db-migrations/infra/typeorm/migrations/1630956212065-CreateTableStage.ts
@@ -1,6 +1,6 @@
 import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
 
-export class CreateTableCreateStage1626209130943 implements MigrationInterface {
+export class CreateTableStage1630956212065 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
@@ -52,4 +52,5 @@ export class CreateTableCreateStage1626209130943 implements MigrationInterface {
         await queryRunner.dropForeignKey('stage', 'stage_FK_construction_config');
         await queryRunner.dropTable('stage');
     }
+
 }
